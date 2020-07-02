6c1aa328bae5f0607129879479108244edd8b3d8
Bart Verthé <bart.verthe@uzgent.be>
2020-07-02 19:20:48 +0200

    test

--- src/GitHubTestLogger/Class1.cs
+++ src/GitHubTestLogger/Class1.cs
@@ -1,5 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Client;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel.Logging;
@@ -15,77 +19,200 @@ namespace GitHubTestLogger {
 
         /// <summary>Alternate user friendly string to uniquely identify the console logger.</summary>
         public const string FriendlyName = "github";
-        public readonly string GITHUB_TOKEN = "";
-        public void Initialize(TestLoggerEvents events, Dictionary<string, string> parameters) {
-            //GITHUB_TOKEN
-            Console.WriteLine("Initialize1-w");
 
-            foreach (var p in parameters) {
-                Console.WriteLine("param: " + p.Key + "=" + p.Value);
+        /// <summary>Name of the Check Run/Job</summary>
+        private const string CheckRunName = "test-report";
+
+        private readonly string[] MandatoryVars = { "GITHUB_REPOSITORY_OWNER", "GITHUB_TOKEN", "GITHUB_SHA", "GITHUB_REPOSITORY" };
+
+        private Dictionary<string, string> Vars;
+
+        private string GITHUB_REPOSITORY_OWNER;
+        private string GITHUB_REPOSITORY_NAME;
+        private string GITHUB_TOKEN;
+        private string GITHUB_SHA;
+        private string GITHUB_WORKSPACE; // /home/runner/work/sandbox/sandbox
+        private CheckRun CurrentCheckRun = null;
+        private GitHubClient gitHubClient = null;
+
+        /// <summary>Concatenate Dictionaries, keep the first KeyValuePair on duplicate Keys</summary>
+        private static Dictionary<string, string> BuildVariables(params Dictionary<string, string>[] args) {
+            var result = new Dictionary<string, string>();
+            foreach (var arg in args) {
+                result = result.Concat(arg.Where(x => !result.ContainsKey(x.Key))).ToDictionary(x => x.Key, x => x.Value);
             }
-            //System.Diagnostics.Debugger.Launch();
-            events.TestRunMessage += Events_TestRunMessage;
-            events.TestResult += this.TestResultHandler;
-            events.TestRunComplete += Events_TestRunComplete;
-            events.TestRunStart += Events_TestRunStart;
-
-            events.DiscoveredTests += Events_DiscoveredTests;
-            //GITHUB_SHA
-            var env = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process);
-            var GITHUB_REPOSITORY_OWNER = env["GITHUB_REPOSITORY_OWNER"].ToString();
-
-            var github = new GitHubClient(new ProductHeaderValue(GITHUB_REPOSITORY_OWNER)) {
-                Credentials = new Credentials(parameters["GITHUB_TOKEN"]) //"a566f1e921b2af225370d55ee114e53d61394e58"),// NOTE: !! real token
-            };
-            var user = github.User.Get("dogguts").Result;
-            Console.WriteLine("/// GotUser:" + user.Name);
-
-            Console.Write("attempt check run creation");
-
-            var check = new NewCheckRun("test-report", env["GITHUB_SHA"].ToString()) {
-                Output = new NewCheckRunOutput("NewCheckRunOutput.Title", "NewCheckRunOutput.Summary") {
-                    Text = "NewCheckRunOutput.Text",
-                },
-                Status = CheckStatus.InProgress,
-                //Conclusion = CheckConclusion.Neutral , 
-            };
-            var checkRun = github.Check.Run.Create("dogguts", "sandbox", check).Result;
-
+            return result;
         }
 
-        private void Events_DiscoveredTests(object sender, DiscoveredTestsEventArgs e) {
-            throw new NotImplementedException();
+        private static (string filename, string methodname, string sourceline) StackFrameFromTrace(string stackTrace) {
+            var s = new StackFrame();
+            var stackLine = stackTrace.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Select(f => f.Trim()).First();
+            var regex = new System.Text.RegularExpressions.Regex(@"(\ *)at (?<method>.*) in (?<filename>.*):line (?<linenumber>\d+)");
+            var match = regex.Match(stackTrace);
+            return (match.Groups["filename"].Value, match.Groups["method"].Value, match.Groups["linenumber"].Value);
         }
 
-        private void Events_TestRunStart(object sender, TestRunStartEventArgs e) {
-            //nouse
-            // throw new NotImplementedException();
-
-        }
-
-        private void Events_TestRunComplete(object sender, TestRunCompleteEventArgs e) {
-            //  throw new NotImplementedException();
-        }
-
-        private void Events_TestRunMessage(object sender, TestRunMessageEventArgs e) {
-            // throw new NotImplementedException();
+        private string WorkspaceRelativePath(string fullPath) {
+            return fullPath.Replace(GITHUB_WORKSPACE, "");
         }
 
         public void Initialize(TestLoggerEvents events, string testRunDirectory) {
-            Console.WriteLine("Initialize2-x");
-            //throw new NotImplementedException();
+            Initialize(events, new Dictionary<string, string>() { { "TestRunDirectory", testRunDirectory } });
+        }
+
+        public void Initialize(TestLoggerEvents events, Dictionary<string, string> parameters) {
+
+            Console.WriteLine("Initialize1-w");
+            Debugger.Launch();
+
+            var environmentVariables = Environment.GetEnvironmentVariables(EnvironmentVariableTarget.Process).ToDictionary<string, string>();
+
+            // Build Vars, parameters take precedence over environment variables
+            Vars = BuildVariables(parameters, environmentVariables);
+
+            // Keep some frequently used Vars 
+            GITHUB_REPOSITORY_OWNER = Vars["GITHUB_REPOSITORY_OWNER"];
+            GITHUB_REPOSITORY_NAME = Vars["GITHUB_REPOSITORY"].Split('/').Last();
+            GITHUB_TOKEN = Vars["GITHUB_TOKEN"];
+            GITHUB_SHA = Vars["GITHUB_SHA"];
+            GITHUB_WORKSPACE = Vars["GITHUB_WORKSPACE"];
+            // connection to github api 
+            gitHubClient = new GitHubClient(new ProductHeaderValue(GITHUB_REPOSITORY_OWNER)) {
+                Credentials = new Credentials(GITHUB_TOKEN)
+            };
+
+            //foreach (var p in Vars) {
+            //    Console.WriteLine("variable: " + p.Key + "=" + p.Value);
+            //}
+
+            // Raised when a test message is received.
+            events.TestRunMessage += Events_TestRunMessage;
+
+            // Raised when a test run starts.
+            events.TestRunStart += Events_TestRunStart;
+
+            // Raised when a test result is received.
+            events.TestResult += Events_TestResult;
+
+            // Raised when a test run is complete.
+            events.TestRunComplete += Events_TestRunComplete;
+
+            // Raised when test discovery starts
+            events.DiscoveryStart += Events_DiscoveryStart;
+
+            // Raised when a discovery message is received.
+            events.DiscoveryMessage += Events_DiscoveryMessage;
+
+            // Raised when discovered tests are received
+            events.DiscoveredTests += Events_DiscoveredTests;
+
+            // Raised when test discovery is complete
+            events.DiscoveryComplete += Events_DiscoveryComplete;
         }
 
 
+        /// <summary>Raised when a test message is received.</summary>
+        private void Events_TestRunMessage(object sender, TestRunMessageEventArgs e) {
+            Console.WriteLine($"*** Events_TestRunMessage *** {e.Level}: {e.Message}");
+        }
 
-        private void TestResultHandler(object sender, TestResultEventArgs e) {
+        /// <summary>Raised when a test run starts.</summary> 
+        private void Events_TestRunStart(object sender, TestRunStartEventArgs e) {
+            Console.WriteLine($"*** Events_TestRunStart ***");
 
-            Console.WriteLine(e.Result.TestCase.FullyQualifiedName + "+++ " + e.Result.Outcome.ToString());
+
+
+            Console.Write("attempt check run creation");
+
+            var check = new NewCheckRun(CheckRunName, GITHUB_SHA) {
+                Output = new NewCheckRunOutput(CheckRunName, "Running...") {
+                    //Text = "NewCheckRunOutput.Text",
+                },
+                Status = CheckStatus.InProgress,
+            };
+            CurrentCheckRun = gitHubClient.Check.Run.Create(GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME, check).Result;
+        }
+
+        /// <summary>Raised when a test result is received.</summary>
+        private void Events_TestResult(object sender, TestResultEventArgs e) {
+            Console.WriteLine($"*** Events_TestResult *** {e.Result.TestCase.FullyQualifiedName}: {e.Result.ErrorMessage}-{e.Result.Outcome} ");
+
+            if (e.Result.Outcome == TestOutcome.Passed || e.Result.Outcome == TestOutcome.None) {
+                //don't annotate successfull tests
+                return;
+            }
+
+            NewCheckRunAnnotation newAnnotation = null;
+            switch (e.Result.Outcome) {
+                case TestOutcome.Failed: // CheckAnnotationLevel.Failure
+                    var (filename, methodname, sourceline) = StackFrameFromTrace(e.Result.ErrorStackTrace);
+                    newAnnotation = new NewCheckRunAnnotation(WorkspaceRelativePath(filename), int.Parse(sourceline), int.Parse(sourceline) + 5, CheckAnnotationLevel.Failure, e.Result.ErrorMessage);
+                    newAnnotation.Title = e.Result.DisplayName;
+                    break;
+                case TestOutcome.Skipped: //CheckAnnotationLevel.Warning
+                    break;
+                case TestOutcome.NotFound: // CheckAnnotationLevel.Failure
+                    break;
+            }
+            if (newAnnotation != null) {
+                var check = new CheckRunUpdate();
+                check.Output.Annotations = new List<NewCheckRunAnnotation>() { newAnnotation };
+                CurrentCheckRun = gitHubClient.Check.Run.Update(GITHUB_REPOSITORY_OWNER, GITHUB_REPOSITORY_NAME, CurrentCheckRun.Id, check).Result;
+            }
+            // update check run with additional annotations
+            //gitHubClient.Check.Run.
+            //var check = new CheckRunUpdate();
+            //  var newAnnotation = new NewCheckRunAnnotation(CheckAnnotationLevel);
+            /*
+TestOutcome:
+        None = 0,
+        Passed = 1,
+        Failed = 2,
+        Skipped = 3,
+        NotFound = 4
+             */
+            /*
+    public enum CheckAnnotationLevel {
+        Notice = 0,
+        Warning = 1,
+        Failure = 2
+    }             
+             */
+
+            //  gitHubClient.Check.Run.Update ()
+
+            Console.WriteLine($"*** Events_TestResult *** {e.Result.TestCase.FullyQualifiedName}: {e.Result.ErrorMessage}-{e.Result.Outcome} ");
+            //Console.WriteLine(e.Result.TestCase.FullyQualifiedName + "+++ " + e.Result.Outcome.ToString());
             //Console.WriteLine(e.Result.TestCase.FullyQualifiedName);
             //Console.WriteLine(e.Result.TestCase.Source);
             //Console.WriteLine(e.Result.TestCase.LineNumber);
         }
 
+        /// <summary>Raised when a test run is complete.</summary>
+        private void Events_TestRunComplete(object sender, TestRunCompleteEventArgs e) {
+            Console.WriteLine($"*** Events_TestRunComplete ***");
+            var xxxx = e.TestRunStatistics.Stats;
 
+        }
+
+        /// <summary>Raised when test discovery starts</summary> 
+        private void Events_DiscoveryStart(object sender, DiscoveryStartEventArgs e) {
+            Console.WriteLine($"*** Events_DiscoveryStart *** {e.DiscoveryCriteria}");
+        }
+
+        /// <summary>Raised when a discovery message is received.</summary>
+        private void Events_DiscoveryMessage(object sender, TestRunMessageEventArgs e) {
+            Console.WriteLine($"*** Events_DiscoveryMessage *** {e.Level}: {e.Message}");
+        }
+
+        /// <summary>Raised when discovered tests are received</summary>
+        private void Events_DiscoveredTests(object sender, DiscoveredTestsEventArgs e) {
+            Console.WriteLine($"*** Events_DiscoveredTests *** {e.DiscoveredTestCases}");
+        }
+
+        /// <summary>Raised when test discovery is complete</summary>
+        private void Events_DiscoveryComplete(object sender, DiscoveryCompleteEventArgs e) {
+            Console.WriteLine($"*** Events_DiscoveryComplete *** {e.TotalCount}");
+        }
     }
 }
---
+++ src/GitHubTestLogger/Extensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace GitHubTestLogger {
+    public static class IDictionaryExtensions {
+        public static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this System.Collections.IDictionary dictionary) {
+            return dictionary.Keys.Cast<TKey>().ToDictionary(key => key, key => (TValue)dictionary[key]);
+        }
+    }
+}
